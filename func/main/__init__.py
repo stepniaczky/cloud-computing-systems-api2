fecf84ceb8b59b6c3c20cc2158ca248a0bf8c939
Mikołaj Stępniak <m.stepniak@yahoo.com>
2022-10-03 23:27:13 +0200

    Initial Azure funcion with fast api & graphql (#1)
    * Initial azure func with fast api & graphql
    
    * Fixing eol
    
    * last fixes to EOL

---
+++ func/main/__init__.py
@@ -0,0 +1,26 @@
+import graphene
+import azure.functions as func
+import nest_asyncio
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from starlette_graphene3 import GraphQLApp, make_graphiql_handler
+
+from schema import Query
+
+nest_asyncio.apply()
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+schema = graphene.Schema(query=Query)
+app.add_route("/api", GraphQLApp(schema=schema,
+              on_get=make_graphiql_handler()))
+
+
+async def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
+    return func.AsgiMiddleware(app).handle(req, context)
